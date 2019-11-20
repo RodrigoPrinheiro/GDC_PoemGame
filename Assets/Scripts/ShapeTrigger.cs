96624553a4739490c94b03103fc92f848d4c7ce0
Miguel Fernandez <miguelrf95@gmail.com>
2019-11-20 12:37:24 +0000

    Add script for a trigger that plays shape change animation

---
+++ Assets/Scripts/ShapeTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ShapeTrigger : MonoBehaviour
+{
+    private Animator control;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            control = other.GetComponent<Animator>();
+            control.Play("BegoneBall");
+            control.Play("GoSquare");
+        }
+    }
+}
