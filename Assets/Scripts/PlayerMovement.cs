7380108b4ef14e76f2d0de1368075d6fea28c143
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-11-20 10:31:47 +0000

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private float _direction;
+    private bool _jumping;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        ComputeVelocity();
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        GetInputs();
+    }
+    private void ComputeVelocity()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+            moveDirection *= speed;
+
+            if (_jumping)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+    }
+
+    private void GetInputs()
+    {
+        _direction = Input.GetAxisRaw("Horizontal");
+        _jumping = Input.GetButton("Jump");
+    }
+}
---
+++ Assets/ShaderScripts/ColorScale.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine;
+
+// Script has the base from https://github.com/Unity-Technologies/PostProcessing/wiki/Writing-Custom-Effects
+[Serializable]
+[PostProcess(typeof(PostProcessColorScaleRenderer), PostProcessEvent.BeforeStack, "PineTree/Color Scale")]
+public sealed class ColorScale : PostProcessEffectSettings
+{
+    // Insert post process settings here
+    [Range(0f, 1f), Tooltip("Colorscale effect intensity.")]
+    public FloatParameter blend = new FloatParameter { value = 0.5f };
+    public ColorParameter darkest = new ColorParameter { value = Color.black};
+    public ColorParameter dark = new ColorParameter { value = Color.grey};
+    public ColorParameter midtones = new ColorParameter { value = Color.grey};
+    public ColorParameter light = new ColorParameter { value = Color.yellow};
+    public ColorParameter lightest = new ColorParameter { value = Color.white};
+
+    public override bool IsEnabledAndSupported(PostProcessRenderContext context)
+    {
+        // Turns off the effect if blend value is 0
+        return enabled.value && blend.value > 0f;
+    }
+}
+
+public sealed class PostProcessColorScaleRenderer : PostProcessEffectRenderer<ColorScale>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/PineTree/ColorScale"));
+        // Set values here
+        sheet.properties.SetFloat("_Blend", settings.blend);
+        sheet.properties.SetColor("_SDarkest", settings.darkest);
+        sheet.properties.SetColor("_SDark", settings.dark);
+        sheet.properties.SetColor("_SMidtones", settings.midtones);
+        sheet.properties.SetColor("_SLight", settings.light);
+        sheet.properties.SetColor("_SLightest", settings.lightest);
+
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
