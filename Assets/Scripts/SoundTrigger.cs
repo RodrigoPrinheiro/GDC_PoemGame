4a22b4965e9f1d225eca86eda615b16249b6c541
Miguel Fernandez <miguelrf95@gmail.com>
2019-11-21 15:38:05 +0000

    Add new folder for sounds. Add soundTrigger class to play sounds when player passes

---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource sound = default;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            sound.Play();
+        }
+    }
+}
